698aad662fafddc9e1a3ef8a1bda8881c37591a2
dortal <dortal@gmail.com>
2015-08-05 17:39:38 +0300

    ass
    ddd

---
+++ test.cs
@@ -0,0 +1,7 @@
+class CodeInjection
+{
+        static void foo(CSharpCodeProvider provider, CompilerParameters compilerParams, TextBox tb)
+        {
+                CompilerResults results = provider.CompileAssemblyFromSource(compilerParams, tb.Text);
+        }
+}
