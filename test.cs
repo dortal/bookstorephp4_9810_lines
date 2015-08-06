2afdc40b2cc24d7174250d3d1fd5dea957e284a2
dortal <dortal@gmail.com>
2015-08-06 11:10:03 +0300

    del
    del

--- test.cs
+++
@@ -1,7 +0,0 @@
-class CodeInjection
-{
-        static void foo(CSharpCodeProvider provider, CompilerParameters compilerParams, TextBox tb)
-        {
-                CompilerResults results = provider.CompileAssemblyFromSource(compilerParams, tb.Text);
-        }
-}
