cef37b818a850d3024902742cdeba05b163566b0
checkmarxQA <dortal@gmail.com>
2015-08-10 13:28:27 +0300

    1

--- full/BookStoreJava_21403_lines/BookDetail_jsp.java
+++ full/BookStoreJava_21403_lines/BookDetail_jsp.java
@@ -8,7 +8,7 @@ public class BookDetail_jsp extends HttpJspBase {
 
 //
 //   Filename: Common.jsp
-//   Generated with CodeCharge  v.1.2.0
+//   Generated with CodeCharge  v.1.2.0   
 //   JSP.ccp build 05/21/2001
 //
 
