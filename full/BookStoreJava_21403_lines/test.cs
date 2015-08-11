b942ea284c187bcc5ba1f3a9d52a70bc6724942a
checkmarxQA <dortal@gmail.com>
2015-08-11 12:32:58 +0300

    0

--- full/BookStoreJava_21403_lines/test.cs
+++
@@ -1,8 +0,0 @@
-//bfgf fdfdfd vdfvfd
-class CodeInjection
-{
-        static void foo(CSharpCodeProvider provider, CompilerParameters compilerParams, TextBox tb)
-        {
-                CompilerResults results = provider.CompileAssemblyFromSource(compilerParams, tb.Text);
-        }
-}
