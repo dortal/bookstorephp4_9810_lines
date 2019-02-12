12a029af4ce7ce17776198363f92215523ecd30f
dortal <dortal@users.noreply.github.com>
2019-02-12 15:56:23 +0200

    test

--- full/test - Copy.cs
+++ full/test - Copy.cs
@@ -1,6 +1,6 @@
 class CodeInjection
 {
-        static void foo(CSharpCodeProvider provider, CompilerParameters compilerParams, TextBox tb)
+        static void foo2(CSharpCodeProvider provider, CompilerParameters compilerParams, TextBox tb)
         {
                 CompilerResults results = provider.CompileAssemblyFromSource(compilerParams, tb.Text);
         }
